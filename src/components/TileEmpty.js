639da37623410df791744032cd155e7eae1542bc
tlnguyen4 <tlnguyen@princeton.edu>
2018-02-04 21:52:25 -0500

    still no input

--- src/components/TileEmpty.js
+++ src/components/TileEmpty.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import './css/TileEmpty.css';
 
 class TileEmpty extends Component {
-    handleChange(event) {
-        event.preventDefault();
-        this.props.numberInput(event.target.value);
-    }
+    // handleChange(event) {
+    //     event.preventDefault();
+    //     this.props.numberInput(event.target.value);
+    // }
 
     render () {
         return (
             <div>
                 {(this.props.correct !== 1) ?
-                    <input className={'number-incorrect'} value={(this.props.value < 0 ? this.props.value * -1 : '')} onChange={(e) => this.handleChange(e)}/> :
-                    <input className={'number-correct'} value={(this.props.value < 0 ? this.props.value * -1 : '')} onChange={(e) => this.handleChange(e)} />}
+                    <input className={'number-incorrect'} value={(this.props.value < 0 ? this.props.value * -1 : '')} onChange={(e) => this.props.numberInput(e.target.value)}/> :
+                    <input className={'number-correct'} value={(this.props.value < 0 ? this.props.value * -1 : '')} onChange={(e) => this.props.numberInput(e.target.value)} />}
             </div>
         )
     }
